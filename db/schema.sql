75898c1ae23d527a82b052079217c37b01473135
sickcurse <111002422+sickcurse@users.noreply.github.com>
2024-09-21 10:55:22 -0400

    added database schema layout for employee management

---
+++ db/schema.sql
@@ -0,0 +1,26 @@
+DROP DATABASE IF EXISTS employees;
+CREATE DATABASE employees;
+\c employees
+
+CREATE TABLE department (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(30) NOT NULL UNIQUE
+);
+
+CREATE TABLE role (
+  id SERIAL PRIMARY KEY,
+  title VARCHAR(30) NOT NULL UNIQUE,
+  salary DECIMAL NOT NULL,
+  department_id INTEGER NOT NULL,
+  FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
+);
+
+CREATE TABLE employee (
+  id SERIAL PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INTEGER NOT NULL,
+  manager_id INTEGER,
+  FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
+  FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
+);
