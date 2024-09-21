8c252bf80b4069ea7ea5fd878ab771f83f2494b8
sickcurse <111002422+sickcurse@users.noreply.github.com>
2024-09-21 10:30:48 -0400

    added getAllEmployees method to retrieve employee data with roles and managers

--- db/index.js
+++ db/index.js
@@ -12,3 +12,14 @@ class Database {
       client.release();
     }
   }
+
+  getAllEmployees() {
+    return this.executeQuery(
+      `SELECT e.id, e.first_name, e.last_name, r.title, d.name AS department, r.salary, 
+      CONCAT(m.first_name, ' ', m.last_name) AS manager 
+      FROM employee e 
+      LEFT JOIN role r ON e.role_id = r.id 
+      LEFT JOIN department d ON r.department_id = d.id 
+      LEFT JOIN employee m ON m.id = e.manager_id;`
+    );
+  }
